8c520a0998a1d8a1ef1ccb516d30c691ebb1487a
Mehulio123 <136765802+Mehulio123@users.noreply.github.com>
2025-03-01 15:56:42 -0500

    chad boi is here

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import './Home.scss';
+import meImage from './me.png'; 
 
 export const Home = () => {
     return (
-        <div className="Home">
+    <div className="Home">
             <h1 className="Home_title">
                 Hello, I am Mehul and <br /> Welcome to my Portfolio!
             </h1>
         {/* About Me Section */}
         <div className="about-me">
             <h2>About Me</h2>
+            <div className="my-picture">
+            <img src={meImage} />
+            </div>
+            <div className="about-me-text">
             <p>
                 I am a passionate developer and designer with experience in React, SCSS, and UI/UX.
                 I love creating beautiful and functional web applications.
@@ -21,7 +26,8 @@ export const Home = () => {
                 I love creating beautiful and functional web applications.
                 I am a passionate developer and designer with experience in React, SCSS, and UI/UX.
             </p>
+            </div>
         </div>
-        </div>
+    </div>
     );
 };
