8bfec60accee5612628fb0608fcf816183d7eb32
Mehulio123 <136765802+Mehulio123@users.noreply.github.com>
2025-03-02 01:29:05 -0500

    personal highlihgts coming in hot

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './Home.scss';
 import meImage from './me.png'; 
+import box1 from './WLU-Hero.png';
+import box2 from './WLU-Hero.png';
+import box3 from './WLU-Hero.png';
 
 export const Home = () => {
     return (
@@ -28,9 +31,43 @@ export const Home = () => {
             </p>
             </div>
         </div>
-        <h1 className="personal-highlights">
-                Personal Highlights
+
+
+        <div className="highlight-container"> {/*this is part of home page, the personal highlight section*/}
+            <h1 className="personal-highlights">
+                    Personal Highlights
             </h1>
+
+            <div className="higlights-content">
+                {/*This is the big image that's clickable*/}
+                <div className="big-box" onClick={() => window.location.href="/Project"}>
+                    <img src={box1} alt="featured project 1"/>
+                    <div className="overlay"> {/* the overlay */}
+                        <p>Project One</p>
+                    </div>
+                </div>
+
+                <div className="two-buttons">
+                    <div className="small-box"  onClick={() => window.location.href="/Project"} >
+                        <img src={box2} alt="featured project 2"/>
+                        <div className="overlay"> {/* the overlay */}
+                            <p>Project Two</p>
+                        </div>
+                    </div>
+
+                    <div className="small-box"  onClick={() => window.location.href="/Project"} >
+                        <img src={box3} alt="featured project 3"/>
+                        <div className="overlay"> {/* the overlay */}
+                            <p>Project three</p>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <button ClassName="contact-button" onClick={()=> window.location.href="./contact"}>
+                Contact me
+            </button>
+
+        </div>
     </div>
     );
 };
