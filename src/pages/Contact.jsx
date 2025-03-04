c627a9a7b800721d028b64c55512ec340fb5c289
Mehulio123 <136765802+Mehulio123@users.noreply.github.com>
2025-03-04 01:14:42 -0500

    contact page roughly

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,10 +3,18 @@ import './Contact.scss';
 
 export const Contact = () => {
     return (  
-        <div className="Contact">
-           <div className="Contact-title">
-            <h1>Contact me!</h1>
-            
+        <div className="contact">
+           <div className="contact-title">
+                <h1>Contact me!</h1>
+                <form>
+                    <p className="prompt">Name:</p>
+                    <input type="text" name="name" placeholder="Enter your name" required/>
+                    <p className="prompt">Email Address:</p>
+                    <input type="email" name="email" placeholder="Enter your email" required />
+                    <p className="prompt">Message:</p>
+                    <textarea className="big-box" name="message" rows="4" placeholder="Write your message here..." required></textarea>
+                    <button className="send" type="submit">Send</button>
+                </form>
             </div> 
         </div> 
     );
