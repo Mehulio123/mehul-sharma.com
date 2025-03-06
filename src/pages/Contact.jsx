abac062f662feb6504147f545f0cbb7a9c254150
Mehulio123 <136765802+Mehulio123@users.noreply.github.com>
2025-03-06 00:32:48 -0500

    almost done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './Contact.scss';
+import box1 from './WLU-Hero.png';
 
 export const Contact = () => {
     return (  
         <div className="contact">
-           <div className="contact-title">
-                <h1>Contact me!</h1>
+           <div className="contact-2">
+                <h1 className="contact-title">Contact me!</h1>
                 <form>
                     <p className="prompt">Name:</p>
                     <input type="text" name="name" placeholder="Enter your name" required/>
@@ -14,8 +15,12 @@ export const Contact = () => {
                     <p className="prompt">Message:</p>
                     <textarea className="big-box" name="message" rows="4" placeholder="Write your message here..." required></textarea>
                     <button className="send" type="submit">Send</button>
-                </form>
-            </div> 
+                </form>  
+                <div className="pic">
+                    <img src={box1} onClick={() => window.location.href="https://www.linkedin.com/in/mehul-sharma-2ba737299/" }/>
+                    <img src={box1} onClick={() => window.location.href="https://www.linkedin.com/in/mehul-sharma-2ba737299/"}/>
+                </div> 
+            </div>
         </div> 
     );
 }
